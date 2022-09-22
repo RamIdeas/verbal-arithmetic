5258d52cd7bd7f95e4e7efbb33a40e05eec5037d
Rahul Sethi <ramideas@users.noreply.github.com>
2022-09-22 13:59:41 +0100

    try add vercel analytics

---
+++ src/analytics.ts
@@ -0,0 +1,53 @@
+import type { Metric } from 'web-vitals';
+import { getCLS, getFCP, getFID, getLCP, getTTFB } from 'web-vitals';
+
+const vitalsUrl = `http://vitals.vercel-insights.com/v1/vitals`;
+
+type AnalyticsOptions = {
+	routeId: string;
+	analyticsId: string;
+	debug?: true;
+};
+
+export function webVitals(options: AnalyticsOptions): void {
+	try {
+		getFID((metric) => sendToAnalytics(metric, options));
+		getTTFB((metric) => sendToAnalytics(metric, options));
+		getLCP((metric) => sendToAnalytics(metric, options));
+		getCLS((metric) => sendToAnalytics(metric, options));
+		getFCP((metric) => sendToAnalytics(metric, options));
+	} catch (err) {
+		console.error('[Analytics]', err);
+	}
+}
+
+function sendToAnalytics(metric: Metric, options: AnalyticsOptions) {
+	const body = {
+		dsn: options.analyticsId,
+		id: metric.id,
+		page: options.routeId,
+		href: location.href,
+		event_name: metric.name,
+		value: metric.value.toString(),
+		speed: getConnectionSpeed()
+	};
+
+	if (options.debug) {
+		console.log('[Analytics]', metric.name, JSON.stringify(body, null, 2));
+	}
+
+	const blob = new Blob([new URLSearchParams(body).toString()], {
+		// This content type is necessary for `sendBeacon`:
+		type: 'application/x-www-form-urlencoded'
+	});
+	if (navigator.sendBeacon) {
+		navigator.sendBeacon(vitalsUrl, blob);
+	} else {
+		fetch(vitalsUrl, {
+			body: blob,
+			method: 'POST',
+			credentials: 'omit',
+			keepalive: true
+		});
+	}
+}
--- svelte.config.js
+++ svelte.config.js
@@ -5,7 +5,12 @@ import preprocess from 'svelte-preprocess';
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess(),
+	preprocess: preprocess({
+		replace: [
+			// this will allow us to use is in the code as import.meta.env.VERCEL_ANALYTICS_ID
+			['import.meta.env.VERCEL_ANALYTICS_ID', JSON.stringify(process.env.VERCEL_ANALYTICS_ID)]
+		]
+	}),
 
 	kit: {
 		adapter: adapter()
